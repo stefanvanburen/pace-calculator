5fc1ae556d32ae2ea7bd24836f7bf64da5df0202
Stefan VanBuren <stefan@vanburen.xyz>
2022-06-28 05:28:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# pace calculator
+
+A simple pace calculator, for calculating running paces.
+
+## Development
+
+Install development dependencies:
+
+```sh
+npm i -D
+```
+
+Run the `shadow-cljs` watcher:
+
+```sh
+npm run dev
+```
+
+Once the watcher is running, open [localhost:8080](http://localhost:8080).
---
+++ src/app/main.cljs
@@ -0,0 +1,128 @@
+(ns app.main
+  {:clj-kondo/config '{:lint-as {reagent.core/with-let clojure.core/let}}}
+  (:require [reagent.dom :as rdom]
+            [reagent.core :as r]))
+
+(defn get-pace [t1 d1 d2]
+  (* t1 (Math/pow (/ d2 d1) 1.06)))
+
+(comment
+  (get-pace (+ 12 (* 60 42)) 10 21)) ; 5559.2493474422945
+
+(defn minutes-from-pace [pace]
+  (-> pace
+      (rem (* 60 60)) ; Stripe the "hour" component
+      (/ 60)          ; Divide seconds by 60 for minutes
+      Math/floor      ; Floor to nearest minute
+      int))           ; Convert to integer
+
+(comment
+  ;; these should equal, because it's 5 minutes and an hour and 5 minutes
+  (minutes-from-pace 300) ; 5
+  (minutes-from-pace (+ (* 60 60) 300))) ; 5
+
+(defn hours-from-pace [pace]
+  (-> pace
+      (/ (* 60 60))
+      Math/floor
+      int))
+
+(comment
+  (hours-from-pace 300) ; 0
+  (hours-from-pace (+ 300 (* 60 60)))) ; 1
+
+(defn seconds-from-pace [pace]
+  (-> pace
+      (rem 60)
+      Math/floor ; Seems reasonable to floor, but could also round
+      int))
+
+(defn format-pace [pace]
+  (when (not= pace "")
+    (str (hours-from-pace pace) ":" (minutes-from-pace pace) ":" (seconds-from-pace pace))))
+
+(comment
+  (format-pace 5559))
+
+(defn total-seconds [hours minutes seconds]
+  (+ seconds (* 60 minutes) (* 60 60 hours)))
+
+(defn calculator
+  []
+  (r/with-let [have-run-distance (r/atom "")
+               have-run-hours (r/atom 0)
+               have-run-minutes (r/atom 0)
+               have-run-seconds (r/atom 0)
+               want-run-distance (r/atom "")
+               pace (r/atom "")]
+
+    [:section
+      [:label {:for "have-run-distance"} "I have run ..."]
+      [:input {:id "have-run-distance"
+               :type "number"
+               :on-change #(reset! have-run-distance (-> % .-target .-value int))}]
+
+      [:fieldset
+       [:legend "In the following time"]
+
+       ; [:label {:for "hours"} "Hours"]
+       [:select
+        {:id "hours"
+         :on-change #(reset! have-run-hours (-> % .-target .-value int))}
+        (for [hour (range 24)]
+          [:option {:value hour
+                    :key (str "hour-" hour)}
+           hour])]
+
+       ":"
+
+       ; [:label {:for "minutes"} "Minutes"]
+       [:select
+        {:id "minutes"
+         :on-change #(reset! have-run-minutes (-> % .-target .-value int))}
+        (for [minute (range 60)]
+          [:option {:value minute
+                    :key (str "minute-" minute)}
+           minute])]
+
+       ":"
+
+       ; [:label {:for "seconds"} "Seconds"]
+       [:select
+        {:id "seconds"
+         :on-change #(reset! have-run-seconds (-> % .-target .-value int))}
+        (for [second (range 60)]
+          [:option {:value second
+                    :key (str "second-" second)}
+           second])]]
+
+      ;; want to run!!!
+      ;; TODO: switch between miles and kilometers
+      [:label {:for "want-run-distance"} "I want to run ..."]
+      [:input {:id "want-run-distance"
+               :type "number"
+               :on-change #(reset! want-run-distance (-> % .-target .-value int))}]
+
+      [:div
+        [:output {:for "pace"} @pace]]
+
+      [:button {:on-click
+                #(let [new-pace (format-pace (get-pace
+                                               (total-seconds @have-run-hours @have-run-minutes @have-run-seconds)
+                                               @have-run-distance
+                                               @want-run-distance))]
+                   (prn @have-run-hours @have-run-minutes @have-run-seconds @have-run-distance @want-run-distance)
+                   (prn new-pace)
+                   (reset! pace new-pace))
+                :id "pace"}
+       "Calculate Pace"]]))
+
+(defn app []
+  [:main
+   [:h1 "Pace Calculator"]
+   [:a {:href "https://github.com/stefanvanburen/pace-calculator"} "Source Code"]
+   [calculator]])
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn ^:export main! []
+  (rdom/render [app] (js/document.querySelector "#app")))
